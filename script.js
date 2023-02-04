11c72df1b55ac4f2eeb33587b56025ba1faeedc0
Dnaeel <daneelshurubkov1@gmail.com>
2023-02-04 14:46:49 +0600

    test

---
+++ script.js
@@ -0,0 +1,44 @@
+/*
+let x = [
+    a = 10,
+    b = 15,
+    c = 9,
+    d = 10
+]
+g = a * b;
+l = c * d;
+let result = g + l;
+console.log(result);
+*/
+
+/****** 2 *******/
+
+/*
+let arr = ['a', 'b', 'c', 'd']
+console.log('a + b, c + d');
+*/
+
+/****** 3 *******/
+
+/*
+let test = 10;
+if (test != 10) {
+    alert(`Верно: ${test}`);
+} else {
+    alert(`Неверно: ${test}`);
+};
+*/
+
+/****** 3.2 *******/
+
+/*
+let test = prompt(10);
+let res = (test == 10);
+if (res){
+    alert(`Неверно: ${res}`);
+}
+let res1 = false;
+if (res1 <= 20){
+    alert(`Верно: ${res1}`);
+}
+*/
